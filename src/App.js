d3e283e5ef4ae1f7f3c95b96c84458b7a305761b
Raajn Patel <rndpatel@gmail.com>
2019-08-14 12:47:23 -0700

    installed yarn/axios
    Able to pull date into console log. Progress!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+    const [title, setTitle] = useState();
+    const [hdurl, setHdurl] = useState();
+    const [url, setUrl] = useState();
+    const [explanation, setExplanation] = useState();
+    const [selectDate, setSelectDate] = useState();
+
+    useEffect(() => {
+        console.log("test");
+        let today = new Date;
+
+        console.log(today.getFullYear());
+        console.log(today.getMonth()+1);
+        console.log(today.getDate());
+    }, [selectDate]);
   return (
     <div className="App">
       <p>
